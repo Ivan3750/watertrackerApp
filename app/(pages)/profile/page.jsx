54872b7211bb93d69fd4ba6794ae9aa54b3e4298
danik00111 <115283667+danik00111@users.noreply.github.com>
2024-11-12 17:56:38 +0200

    create /profile/page.jsx and /settings/page.jsx at app/(pages)/

---
+++ app/(pages)/profile/page.jsx
@@ -0,0 +1,7 @@
+const profilePage =()=> (
+  <div id='profile'>
+    <div id='profile-top'></div>
+    <div id='profile-content'></div>
+  </div>
+);
+export default profilePage;
---
+++ app/(pages)/settings/page.jsx
@@ -0,0 +1,25 @@
+const settingsPage =()=> (
+  <div id='settings'>
+    <div id='settings-header'>
+
+    </div>
+    <div id='settings-content'>
+      <div id="settings-weight">
+
+      </div>
+      <div id="settings-height">
+
+      </div>
+      <div id="settings-waketime">
+
+      </div>
+      <div id="settings-bedtime">
+
+      </div>
+      <div id="settings-units">
+
+      </div>
+    </div>
+  </div>
+);
+export default settingsPage;
